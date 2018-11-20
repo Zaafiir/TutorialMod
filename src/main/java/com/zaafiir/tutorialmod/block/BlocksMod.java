a9b3ac99a69cb374564c446b39e83797850d9b87
Zaafiir <td391@bath.ac.uk>
2018-11-20 19:08:00 +0000

    Test add of folder(block) + class

---
+++ src/main/java/com/zaafiir/tutorialmod/block/BlocksMod.java
@@ -0,0 +1,13 @@
+package com.zaafiir.tutorialmod.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlocksMod extends Block{
+
+	public BlocksMod(Material materialIn) {
+		super(materialIn);
+		// TODO Auto-generated constructor stub
+	}
+
+}
